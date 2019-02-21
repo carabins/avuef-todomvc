a5b18e97bb404bd375f87363ca02a5b908a9452b
Gleb YP <dev@gleb.pw>
2019-02-21 22:19:32 +0300

    v0.1.1

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -3,7 +3,7 @@
     <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone" @change="$a.items.selectAll(allDone)">
     <label for="toggle-all" style="cursor: pointer">Mark all as complete</label>
     <ul class="todo-list">
-      <Item v-for="todo in todos" :key="id" :todo="todo"/>
+      <Item v-for="todo in todos" :key="todo.id" :todo="todo"/>
     </ul>
     <!--<pre > {{Object.keys(todos).map(k=>todos[k])}} </pre>-->
   </section>
